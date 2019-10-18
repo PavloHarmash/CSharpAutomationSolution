b5ca7327b3be64d7ca6f29e33dda6ffe9f14e4fe
Yuriy <yualifanov@deloitte.com>
2019-10-18 11:16:53 +0300

    Added UIBaseTest and JsonHelper classes

--- AutomationTests/Configurations/TestEnvironment/TestEnvConfigs.cs
+++ AutomationTests/Configurations/TestEnvironment/TestEnvConfigs.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutomationTests.Configurations.Environment
+namespace AutomationTests.Configurations.TestEnvironment
 {
-    class EnvironmentConfigs
+    class TestEnvConfigs
     {
     }
 }
